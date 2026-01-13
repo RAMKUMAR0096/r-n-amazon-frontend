45948b28d28531789656b1c6cbf9d5e02a667b9b
Ramkumar0096 <rkum0046@gmail.com>
2026-01-13 16:17:07 +0530

    designing the confirmationScreen

--- screens/ConfirmationScreen.js
+++ screens/ConfirmationScreen.js
@@ -1,11 +1,52 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 const ConfirmationScreen = () => {
+
+  const steps = [
+    { title: "Address", content: "Address Form" },
+    { title: "Delivery", content: "Delivery Options" },
+    { title: "Payment", content: "Payment Details" },
+    { title: "Place Order", content: "Order Summary" },
+  ];
+
+  const [currentStep, setCurrentStep ]=  useState(0);
+
   return (
     <SafeAreaView>
-      <Text>ConfirmationScreen</Text>
+      <ScrollView>
+        <View style={{flex:1,paddingHorizontal:20,paddingTop:40}}>
+          <View style={{flexDirection:"row",alignItems:"center",marginBottom:20,justifyContent:"space-between"}}>
+            {steps.map((item,index)=>(
+              <View key={index} style={{justifyContent:"center",alignItems:"center"}}>
+                {
+                  index >0 && (
+                    <View style={[{flex:1,height:2, backgroundColor:"green"},index<=currentStep && {backgroundColor:"green"}]}/>
+                  )
+                }
+
+                <View style={[{width:30,height:30,borderRadius:15,backgroundColor:"#ccc",justifyContent:"center",alignItems:"center"},index < currentStep && {backgroundColor :"green"}]}>
+                  {
+                    index < currentStep ? (
+                      <Text style={{fontSize:16,fontWeight:"bold",color:"white"}}>&#10003;</Text>
+                    ) :(
+                      <Text>{index +1}</Text>
+                    )
+                  }
+                </View>
+                <Text style={{textAlign:"center",marginTop:8}}>{item.title}</Text>
+              </View>
+            ))}
+          </View>
+        </View>
+
+        { currentStep == 0 && (
+          <View style={{marginHorizontal:20}}>
+            <Text style={{fontSize:16,fontWeight:"bold"}}>Select Delivery Address</Text>
+          </View>
+        )}
+      </ScrollView>
     </SafeAreaView>
   )
 }
